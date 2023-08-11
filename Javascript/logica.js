1247a0b5ca8fb11b719c6479fb62e3097e464d84
lorena perez <lorena879perez@gmail.com>
2023-08-11 10:38:08 -0300

    avance en js

--- Javascript/logica.js
+++ Javascript/logica.js
@@ -1,20 +1,54 @@
 console.table(productos);
+const carro = [];
+let contenedorProds = document.getElementById('misprods');
+let tablaBody = document.getElementById("tablabody");
+let cantidad = document.getElementById("cantidad");
+let prodCarrito = JSON.parse(localStorage.getItem('productos-en-carrito')) || []; 
 
-function filtrarPorPrecio(precioMax) {
-    const filtrados = productos.filter((producto) => producto.precio <= precioMax);
-    console.table(filtrados);
+
+function renderizarProductos(listaProds) {
+    contenedorProds.innerHTML = '';
+    for (const prod of listaProds) {
+        contenedorProds.innerHTML += `
+        <div class="card stylecard" style="width: 18rem;">
+  <img src="${prod.foto}" class="card-img-top" alt="imagen">
+  <div class="card-body">
+    <h5 class="card-title">${prod.nombre}</h5>
+    <p class="card-text">Precio: $${prod.precio}</p>
+    <button id=${prod.id} class="btn btn-primary compra">Comprar</a>
+  </div>
+</div>
+        `;
+    }
+
+    let botones = document.getElementsByClassName("compra");
+    for (const boton of botones) {
+
+        boton.onmouseover = () => boton.classList.replace("btn-primary", "btn-warning");
+        boton.onmouseout = () => boton.classList.replace("btn-warning", "btn-primary")
+    }
 }
 
-let precioUsuario = parseFloat(prompt("Ingresa el precio maximo que puedes abonar (0- Salir)"));
+renderizarProductos(productos);
 
-do{
-    
-    if (isNaN(precioUsuario) || precioUsuario < 0) {
-        alert("Ingrese un numero valido 😒");
-    } else {
-        const prodsFiltrados = filtrarPorPrecio(precioUsuario);
-        console.table(prodsFiltrados);
+document.getElementById("boton-ingresar").addEventListener("click",ingresar)
+function ingresar(){
+    let usuario = document.getElementById("usuario").value;
+
+    if (usuario === ""){
+        alert("No ha ingresado un nombre")
+        return;
+    }else
+    {
+        localStorage.getItem(usuario)
     }
-    precioUsuario = parseFloat(prompt("Ingresa el precio maximo que puedes abonar"));
+}
 
-} while (precioUsuario != 0);
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const getProducts = async () => {
+  const response = await fetch ("data.json");
+  const data = await response.json();
+
+};
+getProducts();
--- Javascript/productos.js
+++ Javascript/productos.js
@@ -1,18 +1,31 @@
-const productos = [{
-    id: 1,
-    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instagram.com%2Fpaulettorey%2F&psig=AOvVaw3rJQsJm2O8RTkyEsVJFBXE&ust=1689950064632000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD16_jwnYADFQAAAAAdAAAAABAE",
-    nombre: "Tapas de empanada Di Pascuale",
-    precio: 200
-},
-{
-    id: 2,
-    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fpaulettorey%2Fphotos%2Fa.322227241665729%2F914101219144992%2F%3Ftype%3D3&psig=AOvVaw3rJQsJm2O8RTkyEsVJFBXE&ust=1689950064632000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD16_jwnYADFQAAAAAdAAAAABAJ",
-    nombre: "Horma de queso azul alloa",
-    precio: 4500
-},
-{
-    id: 3,
-    foto:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.facebook.com%2Fstory.php%3Fstory_fbid%3D867140317174416%26id%3D125020238053098&psig=AOvVaw3rJQsJm2O8RTkyEsVJFBXE&ust=1689950064632000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJD16_jwnYADFQAAAAAdAAAAABAT",
-    nombre: "Jamon cocido La Casona",
-    precio: 2900
-}]
+// const productos = [{
+//     id: 1,
+//     foto:"./img/salame.jpg",
+//     nombre: "Salame paladini",
+//     precio: 2400
+// },
+// {
+//     id: 2,
+//     foto:"./img/manteca1.jpg",
+//     nombre: "Manteca",
+//     precio: 400
+// },
+// {
+//     id: 3,
+//     foto:"./img/queso-cremoso.jpg",
+//     nombre: "Queso cremoso",
+//     precio: 2900
+// },
+// {
+//     id: 4,
+//     foto:"./img/quesoazul.jpg",
+//     nombre: "Queso azul",
+//     precio: 5000
+// },
+// {
+//     id: 4,
+//     foto:"./img/mermelada.jpg",
+//     nombre: "mermelada",
+//     precio: 300
+// }
+// ]
